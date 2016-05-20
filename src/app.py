8f1fc44f203e1a07af91d578b233ffaf5c13b2f6
Abhinav Arora <abhinavdgr8bond@gmail.com>
2016-05-20 06:43:13 -0700

    Heroku Webhook

--- src/app.py
+++ src/app.py
@@ -11,6 +11,8 @@ class MainHandler(RequestHandler):
 
 class WebHookHandler(RequestHandler):
     def get(self):
+
+        print "Chor" + str(self.get_query_argument('hub.verify_token'))
         if len(self.get_query_argument('hub.verify_token')) != 0 and self.get_query_argument('hub.verify_token')[0] == 'TERA_BAAP_KAUN_HAI_BC':
             self.write(self.get_query_argument('hub.challenge'))
         else:
