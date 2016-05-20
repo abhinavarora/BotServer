0d57a99cd72c7a83e4edb34d10d744f2da215bf3
Abhinav Arora <abhinavdgr8bond@gmail.com>
2016-05-20 03:23:06 -0700

    Starter Code

---
+++ src/app.py
@@ -0,0 +1,18 @@
+import tornado.ioloop
+import tornado.web
+from tornado.web import RequestHandler
+
+
+class MainHandler(RequestHandler):
+    def get(self):
+        self.write("Hello, World")
+
+
+def make_app():
+    return tornado.web.Application([(r"/*",MainHandler)])
+
+
+if __name__ == '__main__':
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
