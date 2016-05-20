b23f53521396c1fdb078439022f00ee66d41928b
Abhinav Arora <abhinavdgr8bond@gmail.com>
2016-05-20 05:12:38 -0700

    Adding Webhook Dummy Code

--- src/app.py
+++ src/app.py
@@ -9,20 +9,26 @@ class MainHandler(RequestHandler):
         self.write("Hello, World")
 
 
+class WebHookHandler(RequestHandler):
+    def get(self):
+        print self.get_query_arguments('hub_verify_token')
+
+
 def make_app():
-    return tornado.web.Application([(r"/*",MainHandler)])
+    return tornado.web.Application([(r"/webhook",WebHookHandler)
+        ,(r"/*",MainHandler)])
 
 
 if __name__ == '__main__':
     app = make_app()
 
-    
+
     http_server = tornado.httpserver.HTTPServer(app, ssl_options={
         "certfile": "/etc/ssl/certs/apache-selfsigned.crt",
         "keyfile": "/etc/ssl/private/apache-selfsigned.key"
     })
     http_server.listen(443)
-    
+
     '''
     app.listen(8888)
     '''
